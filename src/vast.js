a734fc77e20f9b6bf7894bc93bd6b56b45a1cea2
Arsen Sepikhanov <a.sepikhanov@dats.team>
2024-01-12 00:29:35 +0400

    main: fix

--- src/vast.js
+++ src/vast.js
@@ -2,24 +2,20 @@
     var player = videojs('vast-video-ok');
     var vastClient = new VAST.VASTClient();
 
-
     vastClient.get('./vast-linear-tag-test.xml')
         .then(function (res) {
             console.log("Successfully parsed xml vast file")
             console.log(res);
 
-            myPlayer.src([
+            player.src([
                 res.ads[0].creatives[0].mediaFiles[0].fileURL
             ]);
 
-            myPlayer.ready(function () {
-                myPlayer.play();
+            player.ready(function () {
+                player.play();
             });
         })
         .catch(function (err) {
             console.log(err);
         });
-
-
-
 }());
